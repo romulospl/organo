0e17a4480e8f1f68d2a448620b075bf44b32ef64
romulospl <romulopereiralopes@gmail.com>
2024-03-13 21:47:11 -0300

    refactor: mudança de cores

--- src/App.js
+++ src/App.js
@@ -10,38 +10,31 @@ function App() {
     [
       {
         nome: 'Programação',
-        corPrimaria: '#57C278',
-        corSecundaria: '#D9F7E9',
+        cor: '#57C278',
       },
       {
         nome: 'Front-End',
-        corPrimaria: '#82CFFA',
-        corSecundaria: '#E8F8FF',
+        cor: '#82CFFA',
       },
       {
         nome: 'Data Sciense',
-        corPrimaria: '#A6D157',
-        corSecundaria: '#F0F8E2',
+        cor: '#A6D157',
       },
       {
         nome: 'Devops',
-        corPrimaria: '#E06B69',
-        corSecundaria: '#FDE7E8',
+        cor: '#E06B69',
       },
       {
         nome: 'UX e Design',
-        corPrimaria: '#D86EBF',
-        corSecundaria: '#FAE5F5',
+        cor: '#D86EBF',
       },
       {
         nome: 'Mobile',
-        corPrimaria: '#FEBA05',
-        corSecundaria: '#FFF5D9',
+        cor: '#FEBA05',
       },
       {
         nome: 'Inovação e Gestão',
-        corPrimaria: '#FF8A29',
-        corSecundaria: '#FFEEDF',
+        cor: '#FF8A29',
       }
     ]
   )
@@ -60,7 +53,7 @@ function App() {
   function mudarCorDoTime(cor, nome) {
     setTimes(times.map(time => {
       if (time.nome === nome) {
-        time.corPrimaria = cor
+        time.cor = cor
       }
       return time
     }))
--- src/components/Time/index.js
+++ src/components/Time/index.js
@@ -1,22 +1,23 @@
 import Colaborador from '../Colaborador'
 import './Time.css'
+import hexToRgba from 'hex-to-rgba';
 
 
 const Time = ({ time, colaboradores, aoDeletar, mudarCor }) => {
     const css = {
-        backgroundColor: time.corSecundaria
+        backgroundColor: hexToRgba(time.cor, '0.6')
     }
 
     return (
         colaboradores.length > 0 && <section className='time' style={css}>
-            <input onChange={evento => mudarCor(evento.target.value, time.nome)} value={time.corSecundaria} type='color' className='input-cor' />
-            <h3 style={{ borderColor: time.corPrimaria }}>{time.nome}</h3>
+            <input onChange={evento => mudarCor(evento.target.value, time.nome)} value={time.cor} type='color' className='input-cor' />
+            <h3 style={{ borderColor: time.cor }}>{time.nome}</h3>
             <div className='colaboradores'>
                 {colaboradores.map(colaborador => {
                     return <Colaborador
                         colaborador={colaborador}
                         key={colaborador.nome}
-                        corDeFundo={time.corPrimaria}
+                        corDeFundo={time.cor}
                         aoDeletar={aoDeletar}
                     />
                 })}
